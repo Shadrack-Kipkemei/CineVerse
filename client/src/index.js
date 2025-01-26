f0f796c5da8629d3fdd5b94b9a8e4df0d4edbe48
sktanui <shadrackkemei2019@gmail.com>
2025-01-26 11:20:17 +0300

    Add Navbar.js, App.js, and index.js for basic app structure and initial setup with React and Bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
-import App from "./components/App";
-import "./index.css";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
