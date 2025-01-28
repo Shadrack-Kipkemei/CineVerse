bcfbfbdf991450942c7b649e610e702ed9dbe11f
sktanui <shadrackkemei2019@gmail.com>
2025-01-28 05:49:11 +0300

    Add database models for User, Movie, and Review with relationships and serialization support using SQLAlchemy and SQLAlchemy-Serializer

---
+++ server/models.py
@@ -0,0 +1,35 @@
+from flask_sqlalchemy import SQLAlchemy 
+from sqlalchemy_serializer import SerializerMixin
+
+db = SQLAlchemy()
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Interger, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+
+    reviews = db.relationship('Review', backref='user', lazy=True)
+
+
+class Movie(db.Model, SerializerMixin):
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    genre = db.Column(db.String, nullable=False)
+    release_year = db.Column(db.Integer, nullable=False)
+    
+    reviews = db.relationship('Review', backref='movie', lazy=True)
+
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.String, nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
