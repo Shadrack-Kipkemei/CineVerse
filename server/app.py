15ac8bcaf41af22b1c871219af823a1506436e54
sktanui <shadrackkemei2019@gmail.com>
2025-01-28 08:34:24 +0300

    add review routs to app.py

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
-from flask_migrate import Migrate 
+from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
 from server.config import Config
 from server.models import db, User, Movie, Review
@@ -21,20 +21,17 @@ class UserListResource(Resource):
 
     def post(self):
         data = request.get_json()
-
-        hashed_password = generate_password_hash(data.get('password'))
+        hashed_password = generate_password_hash(data.get("password"))
         new_user = User(
             username=data.get("username"),
             email=data.get("email"),
-            password=hashed_password 
+            password=hashed_password
         )
         db.session.add(new_user)
         db.session.commit()
-
         return new_user.to_dict(), 201
 
-
-# Movie routes
+# Movie routes (CRUD)
 class MovieListResource(Resource):
     def get(self):
         movies = Movie.query.all()
@@ -47,15 +44,27 @@ class MovieListResource(Resource):
             genre=data.get("genre"),
             release_year=data.get("release_year")
         )
-
         db.session.add(new_movie)
         db.session.commit()
-
         return new_movie.to_dict(), 201
 
+# Review routes
+class ReviewListResource(Resource):
+    def post(self):
+        data = request.get_json()
+        new_review = Review(
+            rating=data.get("rating"),
+            comment=data.get("comment"),
+            movie_id=data.get("movie_id"),
+            user_id=1  
+        )
+        db.session.add(new_review)
+        db.session.commit()
+        return new_review.to_dict(), 201
 
 api.add_resource(UserListResource, '/users')
 api.add_resource(MovieListResource, '/movies')
+api.add_resource(ReviewListResource, '/reviews')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5000, debug=True)
